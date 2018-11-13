5fc53541722932636a8fbc26bc9188ad56aecf7c
Lars Boegild Thomsen <lth@netcompartner.com>
2018-11-13 11:24:22 +0800

    Not working

--- inc/build.mk
+++ inc/build.mk
@@ -2,25 +2,44 @@
 # Makefile for building Subutai Base Templates
 #
 
-# Settings
-ZPOOL=subutai
-ZFS_BUILD=build
-VERSION=0.4.0
-OWNER=lbthomsen
-
 # 
 # Derrived settings
 #
 BUILD=/$(ZPOOL)/$(ZFS_BUILD)
+OS=$(BUILD)/$(NAME)
+ROOTFS=$(BUILD)/$(NAME)/rootfs
+VARFS=$(BUILD)/$(NAME)/$(ROOTFS)/var
+OPTFS=$(BUILD)/$(NAME)/$(ROOTFS)/opt
+HOMEFS=$(BUILD)/$(NAME)/$(ROOTFS)/home
 
-$(BUILD): /$(ZPOOL)
+all: | $(ROOTFS)/etc/
+
+$(ROOTFS)/etc/:  $(HOMEFS)/ $(OPTFS)/ $(VARFS)/
+	@sudo debootstrap $(BASE) /${ROOTFS}
+
+$(HOMEFS)/: $(ROOTFS)/
+	@sudo zfs create $(ZPOOL)/$(ZFS_BUILD)/$(NAME)/rootfs/home
+
+$(OPTFS)/: | $(ROOTFS)/
+	@sudo zfs create $(ZPOOL)/$(ZFS_BUILD)/$(NAME)/rootfs/opt
+
+$(VARFS)/: | $(ROOTFS)/
+	@sudo zfs create $(ZPOOL)/$(ZFS_BUILD)/$(NAME)/rootfs/var
+
+$(ROOTFS)/: | $(OS)/
+	@sudo zfs create $(ZPOOL)/$(ZFS_BUILD)/$(NAME)/rootfs
+
+$(OS)/: | $(BUILD)/
+	@sudo zfs create $(ZPOOL)/$(ZFS_BUILD)/$(NAME)
+
+$(BUILD)/: /$(ZPOOL)/
 	@sudo zfs create $(ZPOOL)/$(ZFS_BUILD)
 
-/$(ZPOOL):
+/$(ZPOOL)/:
 	@echo "The zpool $(ZPOOL) does not exit" ; exit 1
 
 clean: 
-	@sudo zfs destroy $(ZPOOL)/$(ZFS_BUILD)
+	@sudo bash -c '[ -d "$(BUILD)" ] && zfs destroy -r $(ZPOOL)/$(ZFS_BUILD) || true'
 
 .PHONY: all clean
 # vim: ts=8 nowrap autoindent
